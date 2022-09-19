35b44a697cfa46aa97ee317151412348baea2cc9
low-palmera <palmeraslf@gmail.com>
2022-09-18 21:40:18 -0300

    Add files via upload

---
+++ evento-onclick/script.js
@@ -0,0 +1,13 @@
+function turnLogout(element) {
+    element.innerText = "Logout";
+}
+
+// eliminar elemento
+function hide(element) {
+    element.remove();
+}
+
+function alertLike() {
+    // podemos incluir más código aquí si queremos
+    alert('Ninja was liked');
+}
